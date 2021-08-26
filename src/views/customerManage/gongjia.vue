622b6f82b9db7bc22f00b86a2149a2bbb2fcd19c
liuyongli <1598631486@qq.com>
2021-08-26 18:25:35 +0800

    11

---
+++ src/views/customerManage/gongjia.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="drawer" >
+             <el-drawer title="添加客户" :visible.sync="drawer" :with-header="false">
+           <span>客户名称</span>
+           <el-input v-model="input" placeholder="请输入内容"></el-input>
+           <span>客户地址</span>
+           <el-input v-model="input" placeholder="客户地址"></el-input>
+           <el-input type="textarea" :rows="2" placeholder="详细地址" v-model="textarea"></el-input>
+              <el-button class='orangeBtn long1'>确 定</el-button>
+              <el-button class='wuBtn long1'>取 消</el-button>
+            </el-drawer>
+          </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+
+}
+</script>
+
+<style >
+/* 写样式的地方，可以百度一下elementui怎么更改样式，要使用/deep/给组件改样式*/
+.mainbox{
+  width: 1191px;
+  height: 854px;
+  border:red solid 1px;
+  /* border-color: white; */
+  background-color: white;
+  padding: 1rem;
+}
+
+</style>
--- src/views/customerManage/myCustomer.vue
+++ src/views/customerManage/myCustomer.vue
@@ -1,33 +1,58 @@
 <template>
-  <div>
+  <div class="mainbox">
     <!-- 写页面的地方，直接使用elementui组件就可以渲染上去 -->
-    {{ msg }}
+    <div class="Header" >
+      {{ msg }}
+    </div>
     <div>
       <!-- 这个是表单组件，只是用于讲解用法，和实际样式不同 -->
       <el-form :inline="true" :model="formInline" class="demo-form-inline">
-        <el-form-item label="审批人">
-          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
-        </el-form-item>
-        <el-form-item label="活动区域">
-          <el-select v-model="formInline.region" placeholder="活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">查询</el-button>
-        </el-form-item>
-      </el-form>
+  <el-form-item label="客户名称">
+    <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+  </el-form-item>
+  <el-form-item label="客户编码">
+    <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+  </el-form-item>
+  <el-form-item label="上次上单时间">
+    <el-select v-model="formInline.region" placeholder="超过一个月">
+    </el-select>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">查询</el-button>
+    <el-button type="primary" @click="onSubmit">重置</el-button>
+  </el-form-item>
+</el-form>
     </div>
     <div>
       <!-- 这个是表格组件，只是用于讲解用法，和实际样式不同 -->
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180">
-        </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180">
-        </el-table-column>
-        <el-table-column prop="address" label="地址"> </el-table-column>
-      </el-table>
+     <el-table
+      border
+      ref="multipleTable"
+      :data="tableData"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55"></el-table-column>
+    <el-table-column prop="name"  label="客户名称"  width="255"></el-table-column>
+    <el-table-column  prop="code" label="客户编码"  width="138"> </el-table-column>
+     <el-table-column  prop="contacts" label="客户联系人"  width="111"></el-table-column>
+     <el-table-column  prop="number"  label="客户联系电话"  width="137"> </el-table-column>
+    <el-table-column  prop="time"  label="上次下单时间"  width="159"></el-table-column>
+    <el-table-column  prop="address"  label="未核销金额"  width="119"> </el-table-column>
+    <el-table-column  fixed="right"  label="操作"  width="166">
+      <template slot-scope="scope">
+        <el-button @click="handleClick(scope.row)" type="text" size="small">查看详情&ensp;|&ensp;指派业务 </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+    </div>
+    <div>
+      <!-- 分页 -->
+      <el-pagination
+        background
+         layout="total, sizes, prev, pager, next, jumper"
+        :total="150">
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -71,4 +96,12 @@ export default {
 
 <style>
 /* 写样式的地方 */
+.mainbox{
+  width: 1191px;
+  height: 854px;
+  border:red solid 1px;
+  /* border-color: white; */
+  background-color: white;
+  padding: 1rem;
+}
 </style>
