f57a3c3b5742a3d87528dbad85841ea2ad9b5a4d
kanade <work@kumo.work>
2021-10-11 15:06:29 +0800

    车辆管理

--- src/api/configure.js
+++ src/api/configure.js
@@ -81,3 +81,52 @@ export const positionEdit = (params) => {
 }
 // 司机列表
 // driver/lists
+
+// 车辆列表
+export const carLists = (params) => {
+  return axios({
+    url: '/company/car/lists',
+    method: 'post',
+    data: params
+  })
+}
+
+// 添加车辆
+export const carAdd = (params) => {
+  return axios({
+    url: '/company/car/add',
+    method: 'post',
+    data: params
+  })
+}
+
+// 编辑车辆
+export const carEdit = (params) => {
+  return axios({
+    url: '/company/car/edit',
+    method: 'post',
+    data: params
+  })
+}
+
+// 编辑车辆
+export const carEnabled = (carIds) => {
+  return axios({
+    url: '/company/car/enabled',
+    method: 'post',
+    data: {
+      carIds: carIds
+    }
+  })
+}
+
+// 编辑车辆
+export const carDisabled = (carIds) => {
+  return axios({
+    url: '/company/car/disabled',
+    method: 'post',
+    data: {
+      carIds: carIds
+    }
+  })
+}
