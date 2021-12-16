edd0ecdfd65656775237d64481e4d05fffca93c4
bibi <541660090@qq.com>
2021-12-16 16:10:14 +0800

    1

--- src/views/layout/aside/asideComponent/menuItem.vue
+++ src/views/layout/aside/asideComponent/menuItem.vue
@@ -4,8 +4,8 @@
     :index='routerInfo.name'
     :route='{ name: routerInfo.name }'
   >
-    <i :class="'iconfont' + ' ' + routerInfo.meta.icon"></i>
-    <span slot='title' class='menufont'>{{ routerInfo.meta.title }}</span>
+    <i :class="'iconfont' + ' ' + routerInfo.icon"></i>
+    <span slot='title' class='menufont'>{{ routerInfo.title }}</span>
   </el-menu-item>
 </template>
 
